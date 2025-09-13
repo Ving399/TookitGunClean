f818a22206d9aec2ef39d90069353edf743ff139
ving399 <irving19600@gmail.com>
2025-09-13 14:29:03 -0600

    version 3

--- frontend/src/modal.js
+++
@@ -1,7 +0,0 @@
-
-const btnAbrirModal = document.querySelector('#create-button');
-const modal = document.querySelector('#modal');
-
-btnAbrirModal.addEventListener('click', () => {
-    modal.showModal();
-});
